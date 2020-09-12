d8108fef2ab781971f493544d3883d5cc145f936
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2020-09-12 01:22:40 -0500

    Removing Edit route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :previews
+  resources :previews, :except => [:edit]
   root 'previews#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
